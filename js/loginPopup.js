2e3736e7c9c5cc2663b8799f444cc6e2963fba1d
Yasser Kadhim <yasser.kadhim@saabgroup.com>
2020-11-09 13:30:18 +0100

    Add Atlassian login.

---
+++ js/loginPopup.js
@@ -0,0 +1,7 @@
+$('#loginAtlassian').click(function() {  
+    window.open('https://id.atlassian.com/',  'Atlassian Login',
+        "height=640,width=960,toolbar=no,menubar=no,scrollbars=no,location=no,status=no"
+
+    );  
+    return  false;
+});
